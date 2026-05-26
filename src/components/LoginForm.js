323a0b8c0d95acadf34d524750b3287661eabe52
Sagar Mogli <sagarmogli8975@gmail.com>
2026-05-26 15:44:41 +0530

    LoginPage created successfully

---
+++ src/components/LoginForm.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+function LoginForm({ isLoggedIn, onLogin }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (username && password) {
+      onLogin();
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+
+      <br />
+
+      <div>
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+
+      <br />
+
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default LoginForm;
